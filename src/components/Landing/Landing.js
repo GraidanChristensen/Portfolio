22aec0f90711a451896d7db099376217231a921c
GraidanChristensen <graidanc@gmail.com>
2021-02-22 23:56:24 -0700

    portfolio basic setup

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,12 @@
+import react, {Component} from 'react';
+import './Landing.css'
+
+export default class Landing extends Component{
+    render(){
+        return(
+            <div className="Landing">
+                <h1>Landing</h1>
+            </div>
+        )
+    }
+}
