2efbce5069e9a29a49a2405cf20dce551db11efd
GraidanChristensen <graidanc@gmail.com>
2021-03-01 14:22:46 -0700

    contact page icons

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,27 +13,7 @@ export default class About extends Component{
     render(){
         return(
             <div className="About" id="About">
-                <Particles className="particles"
-                    params={{
-                        "particles": {
-                            "number": {
-                                "value": 100
-                            },
-                            "size": {
-                                "value": 5
-                            }
-                        },
-                        "interactivity": {
-                            "events": {
-                                "onHover": {
-                                    "enable": true,
-                                    "mode": "repulse"
-                                }
-                            }
-                        }
-                    }}
-                />
-                <h1>I am a young and ambitious full stack web developer from Utah. I have studied and enjoyed coding and developing for about 3 years now. Any new language, skill, problem or task excites me and motivates me to learn and work hard. When I'm not coding I am on the mountain freestyle skiing or fly fishing. I recently received an Associate of Science degree from Snow College and completed DevMountain's web development course.</h1>
+                <h1>I'm a young and ambitious full stack web developer from Utah. I have studied and enjoyed coding and developing for about 3 years now. Any new language, skill, problem or task excites me and motivates me to learn and work hard. When I'm not coding I am on the mountain freestyle skiing or fly fishing. I recently received an Associate of Science degree from Snow College and completed DevMountain's web development course.</h1>
             </div>
         )
     }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,49 @@
 import react, {Component} from 'react';
 import './Contact.css'
+import linkedin from '../../media/linkedinlogo.png'
+import github from '../../media/githublogo.png'
+import mail from '../../media/mail.png'
+import Particles from 'react-particles-js';
 
 export default class Contact extends Component{
+    constructor(){
+        super();
+        this.state = {
+            
+        }
+    }
+
     render(){
         return(
-            <div className="Contact">
-                <h1>Contact</h1>
+            <div className="Contact" id="Contact">
+                <Particles className="particles"
+                    params={{
+                        "particles": {
+                            "number": {
+                                "value": 100
+                            },
+                            "size": {
+                                "value": 5
+                            }
+                        },
+                        "interactivity": {
+                            "events": {
+                                "onHover": {
+                                    "enable": true,
+                                    "mode": "repulse"
+                                }
+                            }
+                        }
+                    }}
+                />
+                <div className="socialLinks">
+                    <a rel="noreferrer" target="_blank" href="https://github.com/GraidanChristensen"><img className="socialIcon" alt="github" src={github}/></a>
+                    <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/graidan-christensen/"><img className="socialIcon" alt="linkedIn" src={linkedin}/></a>
+                    <a href="mailto:graidanc@gmail.com"><img className="socialIcon" alt="mailto" src={mail}/></a>
+                </div>
             </div>
         )
     }
 }
+
+
