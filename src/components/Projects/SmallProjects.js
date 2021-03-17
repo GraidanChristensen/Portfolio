b4c5b26cf252d331bd3926b35437b4b3a2eb304b
GraidanChristensen <graidanc@gmail.com>
2021-03-17 13:44:17 -0600

    helo added to projects

--- src/components/Projects/SmallProjects.js
+++ src/components/Projects/SmallProjects.js
@@ -1,10 +1,28 @@
 import react, {Component} from 'react';
 import swerveSite from '../../media/swervesite.png'
 import motoring from '../../media/motoring.png'
+import helohome from '../../media/helohome.png'
+import helofeed from '../../media/helofeed.png'
+import helopost from '../../media/helopost.png'
+import arrow from '../../media/arrow.png'
+import {Fade} from 'react-reveal';
 
 import './SmallProjects.css'
 
 export default class SmallProjects extends Component{
+    constructor(){
+        super();
+        this.state={
+            toggleDrop: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            toggleDrop: !this.state.toggleDrop
+        })
+    }
+
     render(){
         return(
             <div className="SmallProjects">
@@ -12,11 +30,23 @@ export default class SmallProjects extends Component{
                     <a rel="noreferrer" target="_blank" href="http://161.35.229.217:4000/#/"><img className="projectImage" alt="swerveSite" src={swerveSite}/></a>
                     <div className="projectContent">
                         <a rel="noreferrer" target="_blank" href="http://161.35.229.217:4000/#/"><h1 className="headingLink">Start Up Eccommerce Website</h1></a>
-                        <h3 className="headingLink">React, Node.js, PostgreSQL, HTML, CSS, Stripe, and Nodemailer</h3>
+                        <h3 className="headingLink">React, Node.js, PostgreSQL, Javascript, HTML, CSS, Stripe, and Nodemailer</h3>
                         <a rel="noreferrer" target="_blank" href="https://github.com/GraidanChristensen/swerve"><img className="github" alt="githublogo" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/></a>
                     </div> 
-                </div>       
+                </div>   
 
+                <div className="projectContainer" onClick={() => {this.handleClick()}}>
+                    <img className="projectImage" alt="helo site" src={helohome}/>
+                    <div className="projectContent">
+                        <h1 onClick={() => {this.handleClick()}} className="headingLink">Social Media Site</h1>
+                        <h3 onClick={() => {this.handleClick()}} className="headingLink">React, Node.js, PostgreSQL, Javascript, CSS</h3>
+                        <a rel="noreferrer" target="_blank" href="https://github.com/GraidanChristensen/helo"><img className="github" alt="githublogo" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/></a>
+                    </div>
+                </div>   
+                    <Fade bottom>
+                    <img onClick={() => {this.handleClick()}} className={this.state.toggleDrop ? "heloImage" : "hideImage"} alt="helo site" src={helofeed}/>
+                    <img onClick={() => {this.handleClick()}} className={this.state.toggleDrop ? "heloImage" : "hideImage"} alt="helo site" src={helopost}/>                               
+                    </Fade>
 
                 <div className="projectContainer">
                     <a rel="noreferrer" target="_blank" href="https://wasatchmotoring.com/"><img className="projectImage" alt="motoSite" src={motoring}/></a>
@@ -24,7 +54,7 @@ export default class SmallProjects extends Component{
                     <a rel="noreferrer" target="_blank" href="https://wasatchmotoring.com/"><h1 className="headingLink">Car Accessory Business Website</h1></a>
                     <a rel="noreferrer" target="_blank" href="https://github.com/GraidanChristensen/Wasatch-Motoring"><img className="github" alt="githublogo" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/></a>
                     </div> 
-                </div>          
+                </div>    
             </div>
         )
     }
